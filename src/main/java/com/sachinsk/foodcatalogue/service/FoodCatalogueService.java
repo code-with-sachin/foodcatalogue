a902d8a1beb3a959dbcbcef5783007f797b3a8a7
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 13:04:55 +0530

    adding save food item logic

--- src/main/java/com/sachinsk/foodcatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/sachinsk/foodcatalogue/service/FoodCatalogueService.java
@@ -1,5 +1,8 @@
 package com.sachinsk.foodcatalogue.service;
 
+import com.sachinsk.foodcatalogue.dto.FoodItemDTO;
+import com.sachinsk.foodcatalogue.entity.FoodItem;
+import com.sachinsk.foodcatalogue.mapper.FoodItemMapper;
 import com.sachinsk.foodcatalogue.repo.FoodItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,10 @@ public class FoodCatalogueService {
 
     @Autowired
     FoodItemRepo foodItemRepo;
+
+    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+        FoodItem foodItemSavedInDB = foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
+
+        return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDB);
+    }
 }
