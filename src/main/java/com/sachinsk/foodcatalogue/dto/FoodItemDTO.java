c7fef92920dbaad391f54b467cca7c43466eb37e
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 12:45:27 +0530

    Adding FoodItemDTO

---
+++ src/main/java/com/sachinsk/foodcatalogue/dto/FoodItemDTO.java
@@ -0,0 +1,19 @@
+package com.sachinsk.foodcatalogue.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodItemDTO {
+
+    private int id;
+    private String item;
+    private String itemDescription;
+    private boolean isVeg;
+    private Number price;
+    private Integer restaurantId;
+    private Integer quantity;
+}
