e45df5dce32ded7aae989bb22bdadffd71a0a7b8
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 12:49:30 +0530

    Adding FoodItemMapper

---
+++ src/main/java/com/sachinsk/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,16 @@
+package com.sachinsk.foodcatalogue.mapper;
+
+import com.sachinsk.foodcatalogue.dto.FoodItemDTO;
+import com.sachinsk.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+
+    FoodItemDTO mapFoodItemToFoodItemDTO(FoodItem foodItem);
+}
