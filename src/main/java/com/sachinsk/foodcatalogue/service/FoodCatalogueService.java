aefd5373966043439465a1cce0e4c2fed899dca8
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 22:34:12 +0530

    modularizing the code

--- src/main/java/com/sachinsk/foodcatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/sachinsk/foodcatalogue/service/FoodCatalogueService.java
@@ -2,64 +2,11 @@ package com.sachinsk.foodcatalogue.service;
 
 import com.sachinsk.foodcatalogue.dto.FoodCataloguePage;
 import com.sachinsk.foodcatalogue.dto.FoodItemDTO;
-import com.sachinsk.foodcatalogue.dto.Restaurant;
-import com.sachinsk.foodcatalogue.entity.FoodItem;
-import com.sachinsk.foodcatalogue.mapper.FoodItemMapper;
-import com.sachinsk.foodcatalogue.repo.FoodItemRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+public interface FoodCatalogueService {
 
-@Service
-public class FoodCatalogueService {
+    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO);
 
-    @Autowired
-    FoodItemRepo foodItemRepo;
+    public FoodCataloguePage fetchFoodCataloguePageDetails(Integer restaurantId);
 
-    @Autowired
-    RestTemplate restTemplate;
-
-    public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
-        FoodItem foodItemSavedInDB = foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
-
-        return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDB);
-    }
-
-    public FoodCataloguePage fetchFoodCataloguePageDetails(Integer restaurantId) {
-
-        //food item list
-        List<FoodItem> foodItemList = fetchFoodItemList(restaurantId);
-        //restaurant details
-        Restaurant restaurantDTO = fetchRestaurantDetailsFromRestaurantMS(restaurantId);
-
-        FoodCataloguePage resultFoodCataloguePage = createFoodCataloguePage(foodItemList, restaurantDTO);
-        return resultFoodCataloguePage;
-    }
-
-    private FoodCataloguePage createFoodCataloguePage(List<FoodItem> foodItemList, Restaurant restaurant) {
-
-        FoodCataloguePage foodCataloguePage = new FoodCataloguePage();
-        foodCataloguePage.setFoodItemList(foodItemList);
-        foodCataloguePage.setRestaurant(restaurant);
-
-        return foodCataloguePage;
-
-    }
-
-    private Restaurant fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
-
-        //We need RestTemplate to fetch the details from another microservice
-       // Note: we will be referring the restaurant listing service's control to find which method returns the details by ID -> in our case is "/fetchById/{id}"
-        Restaurant restaurant = restTemplate.getForObject("http://RESTAURANT-SERVICE/restaurant/fetchById/" +restaurantId, Restaurant.class);
-
-        return restaurant;
-
-    }
-
-    private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
-        List<FoodItem> foodItemListById = foodItemRepo.findByRestaurantId(restaurantId);
-        return foodItemListById;
-    }
 }
