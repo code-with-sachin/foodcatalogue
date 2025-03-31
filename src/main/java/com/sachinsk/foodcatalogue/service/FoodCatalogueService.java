ff3b9c1af43dda9c6f07111855be7ed02607eabb
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 14:06:23 +0530

    creating wireframe design to add logic

--- src/main/java/com/sachinsk/foodcatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/sachinsk/foodcatalogue/service/FoodCatalogueService.java
@@ -1,11 +1,15 @@
 package com.sachinsk.foodcatalogue.service;
 
+import com.sachinsk.foodcatalogue.dto.FoodCataloguePage;
 import com.sachinsk.foodcatalogue.dto.FoodItemDTO;
+import com.sachinsk.foodcatalogue.dto.Restaurant;
 import com.sachinsk.foodcatalogue.entity.FoodItem;
 import com.sachinsk.foodcatalogue.mapper.FoodItemMapper;
 import com.sachinsk.foodcatalogue.repo.FoodItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodCatalogueService {
@@ -17,5 +21,24 @@ public class FoodCatalogueService {
         FoodItem foodItemSavedInDB = foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
 
         return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDB);
+    }
+
+    public FoodCataloguePage fetchFoodCataloguePageDetails(Integer restaurantId) {
+
+        //food item list
+        List<FoodItem> foodItemList = fetchFoodItemList(restaurantId);
+        //restaurant details
+        Restaurant restaurantDTO = fetchRestaurantDetailsFromRestaurantMS(restaurantId);
+
+        createFoodCataloguePage(foodItemList, restaurantDTO);
+    }
+
+    private void createFoodCataloguePage(List<FoodItem> foodItemList, Restaurant restaurantDTO) {
+    }
+
+    private Restaurant fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
+    }
+
+    private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
     }
 }
