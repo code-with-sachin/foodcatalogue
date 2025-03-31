dcc6389079bf1fd664fe9bf87581591db3e96453
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 13:01:17 +0530

    saving food item via controller

--- src/main/java/com/sachinsk/foodcatalogue/controller/FoodCatalogueController.java
+++ src/main/java/com/sachinsk/foodcatalogue/controller/FoodCatalogueController.java
@@ -1,7 +1,12 @@
 package com.sachinsk.foodcatalogue.controller;
 
+import com.sachinsk.foodcatalogue.dto.FoodItemDTO;
 import com.sachinsk.foodcatalogue.service.FoodCatalogueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ public class FoodCatalogueController {
 
     @Autowired
     FoodCatalogueService foodCatalogueService;
+
+    @PostMapping("/addFoodItem")
+    public ResponseEntity<FoodItemDTO> addFoodItem(@RequestBody FoodItemDTO foodItemDTO) {
+        FoodItemDTO savedFoodItem = foodCatalogueService.addFoodItem(foodItemDTO);
+        return new ResponseEntity<>(savedFoodItem, HttpStatus.CREATED);
+    }
 }
