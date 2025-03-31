9abbd5aae9792f216ad825c7418eb47b9b292b3e
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 14:04:30 +0530

    adding get restaurant & food Items by restaurant Id

--- src/main/java/com/sachinsk/foodcatalogue/controller/FoodCatalogueController.java
+++ src/main/java/com/sachinsk/foodcatalogue/controller/FoodCatalogueController.java
@@ -1,14 +1,12 @@
 package com.sachinsk.foodcatalogue.controller;
 
+import com.sachinsk.foodcatalogue.dto.FoodCataloguePage;
 import com.sachinsk.foodcatalogue.dto.FoodItemDTO;
 import com.sachinsk.foodcatalogue.service.FoodCatalogueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/foodCatalogue")
@@ -21,5 +19,15 @@ public class FoodCatalogueController {
     public ResponseEntity<FoodItemDTO> addFoodItem(@RequestBody FoodItemDTO foodItemDTO) {
         FoodItemDTO savedFoodItem = foodCatalogueService.addFoodItem(foodItemDTO);
         return new ResponseEntity<>(savedFoodItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchRestaurantAndFoodItemsById/{id}")
+    public ResponseEntity<FoodCataloguePage> fetchRestaurantDetailsWithFoodMenu(@PathVariable Integer restaurantId) {
+        FoodCataloguePage fetchedFoodCataloguePage = foodCatalogueService.fetchFoodCataloguePageDetails(restaurantId);
+        if (fetchedFoodCataloguePage != null) {
+            return new ResponseEntity<>(fetchedFoodCataloguePage, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 }
